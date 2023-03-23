709a2e662703a952fc0a9b85b304c46f79edb914
8a0k37 <128235765+8a0k37@users.noreply.github.com>
2023-03-23 10:46:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,9 +10,11 @@ count=0
 
 while true; do
   count=((count++))
-  touch /home/${user}/${RANDOM}-${count}
-  mkdir /home/${user}/${RANDOM}-${count}
+  touch /home/${user}/$RANDOM -${count}
+  mkdir /home/${user}/$RANDOM -${count}
   echo 'spammer: successfully made file ${count}.'
   echo "spammer: successfully made dir ${count}."
+  echo 'SPAM: Loop ${count}. tdID = $RANDOM$count$RANDOM'
   sleep 0.01
 done
+
